4c424fa3e5f5b076cbaae6c91e89df52e09d0862
Anastasiya <40817643+Nintendodias@users.noreply.github.com>
2020-12-21 20:40:48 +0300

    create_project

---
+++ project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id="wrapper">
+		<section_menu />
+	</div>
+</template>
+
+<script>
+	import Vue from 'vue';
+	import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
+
+	import 'bootstrap/dist/css/bootstrap.css';
+	import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+	Vue.use(BootstrapVue);
+	Vue.use(BootstrapVueIcons);
+	import section_menu from './components/section_menu.vue';
+
+	export default {
+		name: 'App',
+		components: {
+			section_menu,
+		},
+	};
+</script>
+
+<style lang="less">
+	#wrapper {
+	}
+</style>
---
+++ project/src/components/section_menu.vue
@@ -0,0 +1,23 @@
+<template>
+	<section class="section_menu">
+		
+	</section>
+</template>
+
+<script>
+
+
+	export default {
+		name: 'section_menu',
+		props: {
+			msg: String,
+		},
+	};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+.section_menu {
+	color: red;
+}
+</style>
